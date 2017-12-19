91fc647eb1f74a217001d4853895a59510c2b0ae
Raviajet <praviteja.usapp@gmail.com>
2017-12-19 17:51:42 -0600

    initial commit

---
+++ src/test/java/com/boot/DasBootApplicationTests.java
@@ -0,0 +1,23 @@
+package com.boot;
+
+import com.boot.controller.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DasBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result,"My Controller");
+	}
+
+}
---
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,47 @@
+package com.boot;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+//import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+
+public class ShipwreckControllerTest {
+    @InjectMocks
+    private ShipwreckController sc;
+
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+     @Test
+    public void testShipwreckGet(){
+       // ShipwreckController sc = new ShipwreckController();
+         Shipwreck sw = new Shipwreck();
+          sw.setId(1l);
+         when(shipwreckRepository.findOne(1l)).thenReturn(sw);
+
+         Shipwreck wreck = sc.get(1l);
+
+         verify(shipwreckRepository).findOne(1l);
+
+       // assertEquals(1l,wreck.getId().longValue());
+         assertThat(wreck.getId(), is(1l)); //Hamcrest is much more readable than the above line code
+    }
+}
